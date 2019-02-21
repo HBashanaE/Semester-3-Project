13d7746e4898b7793c09be47fa59d8d6f214a098
Bashana Elikewela <hbashanae@gmail.com>
2019-02-21 21:50:55 +0530

    2019.02.21

---
+++ Loging/authen_login.php
@@ -0,0 +1,26 @@
+<?php  
+ require('db_connect.php');
+
+if (isset($_POST['user_id']) and isset($_POST['user_pass'])){
+	
+// Assigning POST values to variables.
+$username = $_POST['user_id'];
+$password = $_POST['user_pass'];
+
+// CHECK FOR THE RECORD FROM TABLE
+$query = "SELECT * FROM `user_login` WHERE username='$username' and Password='$password'";
+ 
+$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+$count = mysqli_num_rows($result);
+
+if ($count == 1){
+
+//echo "Login Credentials verified";
+echo "<script type='text/javascript'>alert('Login Credentials verified')</script>";
+
+}else{
+echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
+//echo "Invalid Login Credentials";
+}
+}
+?>
---
+++ Loging/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html >
+<html>
+<head>
+<title>MASHLOG LOGIN FORM</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body id="body_bg">
+<div <div align="center">
+
+<h3>MASHLOG LOGIN FORM</h3>
+    <form id="login-form" method="post" action="authen_login.php" >
+        <table border="0.5" >
+            <tr>
+                <td><label for="user_id">User Name</label></td>
+                <td><input type="text" name="user_id" id="user_id"></td>
+            </tr>
+            <tr>
+                <td><label for="user_pass">Password</label></td>
+                <td><input type="password" name="user_pass" id="user_pass"></input></td>
+            </tr>
+			
+            <tr>
+				
+                <td><input type="submit" value="Submit" />
+                <td><input type="reset" value="Reset"/>
+				
+            </tr>
+        </table>
+    </form>
+		</div>
+</body>
+</html>
