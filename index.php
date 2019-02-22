a60decf3af711e4f23dc6676b361449d541c8106
kashyapa_niyarepola <kashyapaniyarepola@gmail.com>
2019-02-22 19:50:14 +0530

    login

--- index.php
+++ index.php
@@ -1,24 +1,47 @@
-<?php 
-include 'Classes/User.php';
+<?php
+session_start();
+
+if($_POST['submit']) {
+	include_once('connection.php');
+	$username = strip_tags($_POST['username']);
+	$password = strip_tags($_POST['password']);
+
+	$sql = "SELECT id,username,password FROM members where username = '$username' LIMIT 1";
+	$query = mysqli_query($db, $sql);
+	if($query) {
+		$row = mysqli_fetch_row($query);
+		$userId= $row[0];
+		$dbUserName = $row[1];
+		$dbPassword = $row[2];
+	}
+	if($username == $dbUserName && $password == $dbPassword) {
+		$_SESSION['username'] = $username;
+		$_SESSION['id'] = $userId;
+		header('Location: user.php');
+	}
+	else {
+		echo "<b><i>Incorrect credentials</i><b>";
+
+	}
+}
+
 ?>
 
-
+///////////////////////////////////////////////////////////////////
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Kuliyata</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
-    <script src="main.js"></script>
+	<title>PHP-SQL Login</title>
 </head>
 <body>
-<?php
-    $user = new User("test1","test1","test1");
-    echo "<br>". $user-> getUsername();
-    ?>
+<h1>Login</h1>
+<form method="post" action="index.php">
+	<input type="text" name = "username" placeholder="Enter username">
+	<input type="password" name="password" placeholder="Enter password here">
+	<input type="submit" name="submit" value="Login">
+</form>
 
+<a href="register.php" >Register</a>
 
 </body>
 </html>
