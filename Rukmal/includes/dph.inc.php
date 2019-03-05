1ca6365c93a9b5bcbc6555fae872c2493ce57ff6
Bashana Elikewela <hbashanae@gmail.com>
2019-03-05 18:13:09 +0530

    Update dph.inc.php

--- Rukmal/includes/dph.inc.php
+++ Rukmal/includes/dph.inc.php
@@ -2,7 +2,7 @@
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
-$dbName = "project_start";
+$dbName = "login";
 $conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName );
 
 
