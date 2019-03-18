8539314e7ffe685b9ff804006a1204f5e9c938f7
Bashana Elikewela <hbashanae@gmail.com>
2019-03-18 22:44:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -164,75 +164,7 @@ if (isset($_SESSION['id'])){
         </div>
     </section>
 
-    <!-- Upload image -->
-    <div class="row">
-        <div class="col-md-6 col-sm-12">
-
-            <!-- Our markup, the important part here! -->
-            <div id="drag-and-drop-zone" class="dm-uploader p-5">
-                <h3 class="mb-5 mt-5 text-muted">Drag &amp; drop files here</h3>
-
-                <div class="btn btn-primary btn-block mb-5">
-                    <span>Open the file Browser</span>
-                    <input type="file" title='Click to add Files' />
-                </div>
-            </div><!-- /uploader -->
-
-        </div>
-        <div class="col-md-6 col-sm-12">
-            <div class="card h-100">
-                <div class="card-header">
-                    File List
-                </div>
-
-                <ul class="list-unstyled p-2 d-flex flex-column col" id="files">
-                    <li class="text-muted text-center empty">No files uploaded.</li>
-                </ul>
-            </div>
-        </div>
-    </div><!-- /file list -->
-
-
-    <div class="row">
-        <div class="col-12">
-            <div class="card h-100">
-                <div class="card-header">
-                    Debug Messages
-                </div>
-
-                <ul class="list-group list-group-flush" id="debug">
-                    <li class="list-group-item text-muted empty">Loading plugin....</li>
-                </ul>
-            </div>
-        </div>
-    </div> <!-- /debug -->
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
-        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <script src="Resources/dist/js/jquery.dm-uploader.min.js"></script>
-    <script src="demo-ui.js"></script>
-    <script src="demo-config.js"></script>
-
-    <!-- File item template -->
-    <script type="text/html" id="files-template">
-    <li class="media">
-        <div class="media-body mb-1">
-            <p class="mb-2">
-                <strong>%%filename%%</strong> - Status: <span class="text-muted">Waiting</span>
-            </p>
-            <div class="progress mb-2">
-                <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" role="progressbar"
-                    style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                </div>
-            </div>
-            <hr class="mt-1 mb-1" />
-        </div>
-    </li>
-    </script>
-
-    <!-- Debug item template -->
-    <script type="text/html" id="debug-template">
-    <li class="list-group-item text-%%color%%"><strong>%%date%%</strong>: %%message%%</li>
-    </script>
+    
 
 </body>
 
