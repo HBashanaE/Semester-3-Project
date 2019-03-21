8d32fbb92dcec1d6f599ed57f013045f50bab50e
Bashana Elikewela <hbashanae@gmail.com>
2019-03-21 21:48:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,66 +1,68 @@
-< !DOCTYPE html><?php  //$logged=false;
-                $show_modal = false;
-                session_start();
+< !DOCTYPE html>
+    <?php  //$logged=false;
+    $show_modal = false;
+    session_start();
 
-                if (isset($_POST['submit'])) {
-                    if ($_POST['submit']) {
-                        include_once('connection.php');
-                        $username = strip_tags($_POST['username']);
-                        $password = strip_tags($_POST['password']);
+    if (isset($_POST['submit'])) {
+        if ($_POST['submit']) {
+            include_once('connection.php');
+            $username = strip_tags($_POST['username']);
+            $password = strip_tags($_POST['password']);
 
-                        $sql = "SELECT activated, username, password FROM members where username = '$username' LIMIT 1";
-                        $query = mysqli_query($db_login, $sql);
+            $sql = "SELECT activated, username, password FROM members where username = '$username' LIMIT 1";
+            $query = mysqli_query($db_login, $sql);
 
-                        if ($query) {
-                            $row = mysqli_fetch_row($query);
-                            $userId = $row[2];
-                            $dbUserName = $row[1];
-                            $dbPassword = $row[2];
-                        }
+            if ($query) {
+                $row = mysqli_fetch_row($query);
+                $userId = $row[2];
+                $dbUserName = $row[1];
+                $dbPassword = $row[2];
+            }
 
-                        if (!empty($_POST['username'])) {
-                            if ($username == $dbUserName && $password == $dbPassword) {
-                                $_SESSION['username'] = $username;
-                                $_SESSION['id'] = $userId;
-                                header('Location: index.php');
-                            } else {
-                                echo "<b><i>Invalid password</i><b>";
-                            }
-                        } else {
-                            $show_modal = true;
-                        }
-                    }
-                }
-
-                //only if session is created then user has logged in
-                if (isset($_SESSION['id'])) {
-                    $userId = $_SESSION['id'];
-                    $username = $_SESSION['username'];
-                    $logged = true;
+            if (!empty($_POST['username'])) {
+                if ($username == $dbUserName && $password == $dbPassword) {
+                    $_SESSION['username'] = $username;
+                    $_SESSION['id'] = $userId;
+                    header('Location: index.php');
                 } else {
-                    $logged = false;
+                    echo "<b><i>Invalid password</i><b>";
                 }
+            } else {
+                $show_modal = true;
+            }
+        }
+    }
 
-                ?>< !-- Image Upload --><?php $msg = "";
+    //only if session is created then user has logged in
+    if (isset($_SESSION['id'])) {
+        $userId = $_SESSION['id'];
+        $username = $_SESSION['username'];
+        $logged = true;
+    } else {
+        $logged = false;
+    }
 
-                        if (isset($_POST['upload'])) {
-                            $target = "alladds/" . basename($_FILES['image']['name']);
-                            $db = mysqli_connect("localhost", "root", "", "login");
-                            $image = $_FILES['image']['name'];
-                            $text = $_POST['text'];
-                            $TNum = $_POST['TNum'];
-                            $Price = $_POST['Price'];
-                            $sql = "INSERT INTO alladds(image, text,price,pnum) VALUES ('$image','$text','$TNum','$Price');";
-                            mysqli_query($db, $sql);
+    ?>< !-- Image Upload -->
+        <?php $msg = "";
 
-                            if (move_uploaded_file($_FILES['image']['name'], $target)) {
-                                $msg = "image uploaded successfully";
-                            } else {
-                                $msg = "There was a problem uploading image";
-                            }
-                        }
+        if (isset($_POST['upload'])) {
+            $target = "alladds/" . basename($_FILES['image']['name']);
+            $db = mysqli_connect("localhost", "root", "", "login");
+            $image = $_FILES['image']['name'];
+            $text = $_POST['text'];
+            $TNum = $_POST['TNum'];
+            $Price = $_POST['Price'];
+            $sql = "INSERT INTO alladds(image, text,price,pnum) VALUES ('$image','$text','$TNum','$Price');";
+            mysqli_query($db, $sql);
 
-                        ?><html>
+            if (move_uploaded_file($_FILES['image']['name'], $target)) {
+                $msg = "image uploaded successfully";
+            } else {
+                $msg = "There was a problem uploading image";
+            }
+        }
+
+        ?><html>
 
         <head>
             <meta charset="ISO-8859-1">
@@ -82,7 +84,8 @@
                             <script src="Resources/fileUploader/dropzone.js"></script>
                             < !-- <link href="http://hayageek.github.io/jQuery-Upload-File/4.0.11/uploadfile.css" rel="stylesheet">
                                 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-                                <script src="http://hayageek.github.io/jQuery-Upload-File/4.0.11/jquery.uploadfile.min.js"></script>-->
+                                <script src="http://hayageek.github.io/jQuery-Upload-File/4.0.11/jquery.uploadfile.min.js">
+                                </script>-->
                                 <link href="Resources/dist/css/jquery.dm-uploader.min.css" rel="stylesheet">
                                 <link href="Resoures/styles.css" rel="stylesheet">
         </head>
@@ -95,9 +98,9 @@
                             <div class="generel_user" id="div_generel_user" <?php if ($logged === true) {
                                                                                 ?>style="display:none" <?php
 
-                        }
+                                            }
 
-                        ?>>
+                                            ?>>
                                 <div class="row">
                                     < !-- Login form -->
                                         <form method="post" action="index.php" class="form-inline " style="content-right"><input type="text" name="username" class="form-control mr-sm-2" placeholder="Username or email"><input type="password" name="password" class="form-control mr-sm-2" placeholder="Password"><button type="submit" name="submit" class="float btn btn-outline-info my-2 my-sm-0 mr-sm-2 mr-xs-1 my-xs-0" value="login" data-toggle="modal" data-target=".bd-example-modal-sm">Login</button></form>
@@ -118,11 +121,17 @@
                                         <div class="logged_user" id="div_logged_user" <?php if ($logged === false) {
                                                                                             ?>style="display:none" <?php
 
-                        }
+                                                        }
 
-                        ?>>
-                                            <div class="btn-group"><button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false"><?php echo htmlentities($username) ?></button>
-                                                <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" type="button">Account</a><a class="dropdown-item" type="button">Another action</a><a class="dropdown-item" type="button" href="logout.php">Logout</a></div><button type="button" class="btn btn-warning">Warning</button>
+                                                        ?>>
+                                            <div class="btn-group">
+                                            <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false"><?php echo htmlentities($username) ?></button>
+                                                <div class="dropdown-menu dropdown-menu-right">
+                                                <a class="dropdown-item" type="button">Account</a>
+                                                <a class="dropdown-item" type="button">Another action</a>
+                                                <a class="dropdown-item" type="button" href="logout.php">Logout</a>
+                                                </div>
+                                                <button type="button" class="btn btn-warning">Warning</button>
                                             </div>
                                         </div>
                 </nav>
@@ -133,7 +142,8 @@
                                 <div class="row align-items-center">
                                     <div class="col-lg-12">
                                         <div class="row align-items-center">
-                                            <div class="col-lg-5 col-md-5 col-sm-12 p-2"><input type="text" name="query" class="form-control search-slt" placeholder="What do you want?"></div>
+                                            <div class="col-lg-5 col-md-5 col-sm-12 p-2"><input type="text" name="query" class="form-control search-slt" placeholder="What do you want?">
+                                            </div>
                                             <div class="col-lg-5 col-md-5 col-sm-12 p-2"><select class="form-control search-slt" name="category" id="selectCategory" placeholder="Select Category">
                                                     <option>Vehicles</option>
                                                     <option>Cleaning appliences</option>
@@ -151,25 +161,24 @@
                             </form>
                         </div>
                     </section>
-                    <div id="sub_content"><?php  // include_once('connection.php');
-                                            // $sql = "SELECT * FROM ads";
-                                            // $result = mysqli_query($db_login,$sql);
-                                            // while ($row = mysqli_fetch_array($result)){
-                                            // 		echo "<div class='card mb-2'>";
-                                            // 			echo "<img src='ads/".$row['images']."' class='card-img-top' alt='image'>";
-                                            // 				echo "<div class='card-body'>";
-                                            // 					echo "<h5 class='card-title'>".$row['title']."</h5>";
-                                            // 				echo "<p class='card-text'>".$row['description']."</p>";
-                                            // 				echo "<p class='card-text'><small class='text-muted'>Last updated 3 mins ago</small></p>";
-                                            // 				echo "</div>";
-                                            // 		echo "</div>";
-                                            //}
+                    <div id="sub_content">
+                        <?php  // include_once('connection.php');
+                        // $sql = "SELECT * FROM ads";
+                        // $result = mysqli_query($db_login,$sql);
+                        // while ($row = mysqli_fetch_array($result)){
+                        // 		echo "<div class='card mb-2'>";
+                        // 			echo "<img src='ads/".$row['images']."' class='card-img-top' alt='image'>";
+                        // 				echo "<div class='card-body'>";
+                        // 					echo "<h5 class='card-title'>".$row['title']."</h5>";
+                        // 				echo "<p class='card-text'>".$row['description']."</p>";
+                        // 				echo "<p class='card-text'><small class='text-muted'>Last updated 3 mins ago</small></p>";
+                        // 				echo "</div>";
+                        // 		echo "</div>";
+                        //}
 
-                                            ?></div>
-                    <div class="card mb-3" style="height: 18rem; width:50rem;"><img src="Resources/kuliyata_logo_full.png" class="card-img-top img-responsive" style="width:auto;
-height:10rem;
-
-" alt="...">
+                        ?>
+                    </div>
+                    <div class="card mb-3" style="height: 18rem; width:50rem;"><img src="Resources/kuliyata_logo_full.png" class="card-img-top img-responsive" style="width:auto; height:10rem;" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Test Ad</h5>
                             <p class="card-text">This is ad description.</p>
@@ -179,16 +188,18 @@ height:10rem;
                     < !-- <div class="card mb-3"><img src="..." class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
                             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                         </div>
                         </div>-->< !-- Floating button -->
                             <div <?php if ($logged === false) {
                                         ?>style="display:none" <?php
 
-                        }
+                    }
 
-                        ?>><a href="#" class="float" style="position:fixed;
+                    ?>>
+                    <a href="#" class="float" style="position:fixed;
 width:60px;
 height:60px;
 bottom:40px;
