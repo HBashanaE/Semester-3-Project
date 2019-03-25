97470b5bb00d8f776605d3d6b66612cf770d2fb0
Bashana Elikewela <hbashanae@gmail.com>
2019-03-26 05:22:55 +0530

    Update bootstrap.php

--- MVC/core/bootstrap.php
+++ MVC/core/bootstrap.php
@@ -5,12 +5,12 @@ require_once(ROOT.DS.'app'.DS.'lib'.DS.'helpers'.DS.'functions.php');
 
 //Autoload classes
 function __autoload($className){
-    if(file_exists(ROOT.DS.'core'.DS.$className.'php')){
-        require_once(ROOT.DS.'core'.DS.$className.'php');
-    }elseif(file_exists(ROOT.DS.'app'.DS.'controllers'.DS.$className.'php')){
-        require_once(ROOT.DS.'app'.DS.'controllers'.DS.$className.'php');
-    }elseif(file_exists(ROOT.DS.'app'.DS.'models'.DS.$className.'php')){
-        require_once(ROOT.DS.'app'.DS.'models'.DS.$className.'php');
+    if(file_exists(ROOT.DS.'core'.DS.$className.'.php')){
+        require_once(ROOT.DS.'core'.DS.$className.'.php');
+    }elseif(file_exists(ROOT.DS.'app'.DS.'controllers'.DS.$className.'.php')){
+        require_once(ROOT.DS.'app'.DS.'controllers'.DS.$className.'.php');
+    }elseif(file_exists(ROOT.DS.'app'.DS.'models'.DS.$className.'.php')){
+        require_once(ROOT.DS.'app'.DS.'models'.DS.$className.'.php');
     }
 }
 
