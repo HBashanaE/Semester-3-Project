e2959882657d01aa41c1b06d893b5ffa239c8e60
Bashana Elikewela <hbashanae@gmail.com>
2019-04-12 12:21:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -50,13 +50,14 @@ if (isset($_SESSION['id'])) {
     <title>කුළියට</title>
     <link rel="shortcut icon" href="Resources/favicon.ico">
     <!-- Bootstrap CDN-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -184,8 +185,7 @@ if (isset($_SESSION['id'])) {
         ?>
     </div>
     <div class="card mb-3" style="height: 18rem; width:50rem;">
-        <img src="Resources/kuliyata_logo_full.png" class="card-img-top img-responsive" style="width:auto;
-  height:10rem;" alt="...">
+        <img src="Resources/kuliyata_logo_full.png" class="card-img-top img-responsive" style="width:auto;height:10rem;" alt="...">
         <div class="card-body">
             <h5 class="card-title">Test Ad</h5>
             <p class="card-text">This is ad description.</p>
@@ -203,8 +203,7 @@ if (isset($_SESSION['id'])) {
             </div>
         </div> -->
     <!-- Floating button -->
-    <div <?php if ($logged === false) { ?>style="display:none" <?php 
-                                                        } ?>>
+    <div <?php if ($logged === false) { ?>style="display:none" <?php } ?>>
         <a href="upload.php"class="float"style="position:fixed;
         width:60px;
         height:60px;
